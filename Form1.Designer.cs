903db3c12490b920e7a79c115cca444f79ed5562
pawbabyDE <92847651+pawbabyDE@users.noreply.github.com>
2022-05-25 19:40:38 +0200

    kto nie skacze ten z HR'u

---
+++ Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace FunkcjaKwadratowa
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lb_answer1 = new System.Windows.Forms.Label();
+            this.lb_answer2 = new System.Windows.Forms.Label();
+            this.lb_answer3 = new System.Windows.Forms.Label();
+            this.tb_a = new System.Windows.Forms.TextBox();
+            this.tb_b = new System.Windows.Forms.TextBox();
+            this.tb_c = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 67);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(13, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 67);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "X2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(13, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 67);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Delta";
+            // 
+            // lb_answer1
+            // 
+            this.lb_answer1.AutoSize = true;
+            this.lb_answer1.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lb_answer1.Location = new System.Drawing.Point(213, 18);
+            this.lb_answer1.Name = "lb_answer1";
+            this.lb_answer1.Size = new System.Drawing.Size(0, 67);
+            this.lb_answer1.TabIndex = 3;
+            // 
+            // lb_answer2
+            // 
+            this.lb_answer2.AutoSize = true;
+            this.lb_answer2.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lb_answer2.Location = new System.Drawing.Point(213, 93);
+            this.lb_answer2.Name = "lb_answer2";
+            this.lb_answer2.Size = new System.Drawing.Size(0, 67);
+            this.lb_answer2.TabIndex = 4;
+            // 
+            // lb_answer3
+            // 
+            this.lb_answer3.AutoSize = true;
+            this.lb_answer3.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lb_answer3.Location = new System.Drawing.Point(213, 158);
+            this.lb_answer3.Name = "lb_answer3";
+            this.lb_answer3.Size = new System.Drawing.Size(0, 67);
+            this.lb_answer3.TabIndex = 5;
+            // 
+            // tb_a
+            // 
+            this.tb_a.Location = new System.Drawing.Point(52, 239);
+            this.tb_a.Name = "tb_a";
+            this.tb_a.Size = new System.Drawing.Size(100, 23);
+            this.tb_a.TabIndex = 6;
+            // 
+            // tb_b
+            // 
+            this.tb_b.Location = new System.Drawing.Point(52, 278);
+            this.tb_b.Name = "tb_b";
+            this.tb_b.Size = new System.Drawing.Size(100, 23);
+            this.tb_b.TabIndex = 7;
+            // 
+            // tb_c
+            // 
+            this.tb_c.Location = new System.Drawing.Point(52, 319);
+            this.tb_c.Name = "tb_c";
+            this.tb_c.Size = new System.Drawing.Size(100, 23);
+            this.tb_c.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 247);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "A)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 281);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "B)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 319);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 15);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "C)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(52, 348);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 53);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Rozwiąż";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox1.Location = new System.Drawing.Point(512, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(285, 168);
+            this.richTextBox1.TabIndex = 13;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tb_c);
+            this.Controls.Add(this.tb_b);
+            this.Controls.Add(this.tb_a);
+            this.Controls.Add(this.lb_answer3);
+            this.Controls.Add(this.lb_answer2);
+            this.Controls.Add(this.lb_answer1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label lb_answer1;
+        private Label lb_answer2;
+        private Label lb_answer3;
+        private TextBox tb_a;
+        private TextBox tb_b;
+        private TextBox tb_c;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Button button1;
+        private RichTextBox richTextBox1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿namespace FunkcjaKwadratowa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                double a, b, c, ans1, ans2, ans3;
+                a = double.Parse(tb_a.Text);
+                b = double.Parse(tb_b.Text);
+                c = double.Parse(tb_c.Text);
+                ans1 = (-1 * b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a);
+                ans2 = (-1 * b - Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a);
+                ans3 = (Math.Pow(b, 2) - 4 * a * c);
+                lb_answer1.Text = ans1.ToString("n2");
+                lb_answer2.Text = ans2.ToString("n2");
+                lb_answer3.Text = ans3.ToString("n2");
+                if (ans3 > 0)
+                {
+
+
+                    this.richTextBox1.Text = "\nDelta jest większa od 0:\n"
+                    + "x1 = (-b - √Δ) / 2a ≈ " + Math.Round(ans1) + "\nx2 = (-b + √Δ) / 2a ≈ " + Math.Round(ans2);
+                }
+                else if (ans3 < 0)
+                {
+
+                    richTextBox1.Text = "\nDelta jest mniejsza od 0: Brak Rozwiązań dla x1 i x2\n";
+
+                }
+                else if (ans3 == 0)
+                {
+                    double x0;
+                    x0 = -b / (2 * a);
+                    richTextBox1.Text = "\nDelta Jest Równa 0:\n"
+                    + "xₒ = x1 = x2 = -b / 2a ≈ " + Math.Round(ans1);
+                }
+
+            }
+        }
+    }
+}
